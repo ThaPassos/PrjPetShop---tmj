2d0acd79411c13a3802681f36d63fe944249f1ee
Thafany Passos <thafany.passos.dev20242gmail.com>
2025-04-03 09:46:07 -0300

    Adicionando todos os arquivos do projeto

---
+++ src/scripts/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 80,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      }
+    }
+  });
